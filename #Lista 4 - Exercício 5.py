00b7be230ea7f37897a3c5df2674f6705337e609
desjoao <98698809+desjoao@users.noreply.github.com>
2023-10-24 22:31:54 -0300

    Add files via upload

---
+++ #Lista 4 - Exercício 5.py
@@ -0,0 +1,5 @@
+#Lista 4 - Exercício 5
+print('Os números compreendidos entre 1000 e 1999 que divididos por 11 possuem resto 5 são: ')
+for i in range(1000, 2000):
+    if i%11==5:
+        print(i)
